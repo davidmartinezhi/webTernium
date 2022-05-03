36e1333698a1775c735514f05f3c2bcb226d87eb
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-05-03 17:30:55 -0500

    Agregado estilo

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import React, { useState, useEffect } from "react";
 import Unity, { UnityContext } from "react-unity-webgl";
 import NavBar from "./components/NavBar";
 import Modal from "./components/Modal";
-import Sider from "./components/Sider";
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 
 const unityContext = new UnityContext({
-  loaderUrl: "../unity/intento2Mouse.loader.js",
-  dataUrl: "../unity/intento2Mouse.data",
-  frameworkUrl: "../unity/intento2Mouse.framework.js",
-  codeUrl: "../unity/intento2Mouse.wasm",
+  loaderUrl: "../unity/MapadeCalor.loader.js",
+  dataUrl: "../unity/MapadeCalor.data",
+  frameworkUrl: "../unity/MapadeCalor.framework.js",
+  codeUrl: "../unity/MapadeCalor.wasm",
 });
 
 function App() {
@@ -19,36 +18,35 @@ function App() {
 
   //Change Filters On Heatmap
   const unityFilterController = (filter) => {
-    unityContext.send("JS_Handler", "RecibeWeb", filter);
+    unityContext.send("JSHandler", "setfiltros", filter);
     console.log("Unity filter controller...");
   };
 
   //Change areas on heatmap
   const unityAreaController = (area) => {
-    console.log(area);
-    //unityContext.send("JSHandler", "RecibeWeb", area);
+    
+    unityContext.send("JSHandler", "SetArea", area);
+    console.log("Unity area controller...");
   };
 
   //Alert detection
-  useEffect(() => {
-    unityContext.on("SendAlert", (unityAlert) => {
-      console.log(unityAlert);
-      setAlert({unityAlert});
-      setIsVisible(true);
-    })
-    setAlert({});
-  }, []);
+  // useEffect(() => {
+  //   unityContext.on("SendAlert", (unityAlert) => {
+  //     setAlert({unityAlert});
+  //     setIsVisible(true);
+  //   })
+  //   setAlert({});
+  // }, []);
 
   return (
     <>
       <div className="header">
-        <NavBar filterController={unityFilterController} />
+        <NavBar filterController={unityFilterController} unityAreaController={unityAreaController} />
       </div>
       <div className="content">
         <Unity
           unityContext={unityContext}
           style={{
-            color: "rgba(255, 255, 255, 0.10)",
             position: "fixed",
             height: "100vh",
             width: "100%",
@@ -57,7 +55,7 @@ function App() {
             display: "flex",
           }}
         />
-        <Modal alert={alert} setIsVisible={setIsVisible} isVisible={isVisible} />
+        {/* <Modal alert={alert} setIsVisible={setIsVisible} isVisible={isVisible} /> */}
       </div>
     </>
   );
