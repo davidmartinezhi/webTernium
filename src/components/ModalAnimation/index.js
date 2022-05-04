7d15ad700ba10e92c639e0167b24ed8fa268a82c
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-05-04 11:10:08 -0500

    Proyecto terminado

--- src/components/ModalAnimation/index.js
+++ src/components/ModalAnimation/index.js
@@ -1,30 +1,47 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleInfo } from "@fortawesome/free-solid-svg-icons";
 import { Modal as BootstrapModal, Button } from "react-bootstrap";
 import ReactPlayer from "react-player";
 import animation from "../../assets/video/mp4/TerniumAnimation.mp4";
+import clock from "../../assets/video/mp4/Clock.mp4"
 import "./ModalAnimation.css";
 
 export default function ModalAnimation(props) {
     const { isVisible, setIsVisible } = props;
 
-
+ 
   return (
     <>
-      <BootstrapModal centered size="xl" show={isVisible} onHide={() => setIsVisible(false)}>
+      <BootstrapModal
+        centered
+        size="xl"
+        show={isVisible}
+        onHide={() => setIsVisible(false)}
+      >
         <BootstrapModal.Header closeButton>
-          <BootstrapModal.Title> <FontAwesomeIcon icon={faCircleInfo} /> Comportamiento en el transcurso del día</BootstrapModal.Title>
+          <BootstrapModal.Title>
+            {" "}
+            <ReactPlayer
+            url={clock}
+            playing={true}
+            loop={true}
+            controls={false}
+            width="30px"
+            height="30px"
+          />
+            Comportamiento en el transcurso del día 
+
+          </BootstrapModal.Title>
+
         </BootstrapModal.Header>
         <BootstrapModal.Body>
-            <ReactPlayer 
-                  url={animation}
-                  playing={true} 
-                  loop={true} 
-                  controls={false}
-                  width="100%"
-                  height="100%"
-            />
+          <ReactPlayer
+            url={animation}
+            playing={true}
+            loop={true}
+            controls={false}
+            width="100%"
+            height="100%"
+          />
         </BootstrapModal.Body>
         <BootstrapModal.Footer>
           <Button variant="warning" onClick={() => setIsVisible(false)}>
